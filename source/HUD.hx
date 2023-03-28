8c9788e44136a233b0afe872903519e9f9fd66e9
Richard Peterson <peterson4487@live.missouristate.edu>
2023-03-28 18:18:43 -0500

    Reworking HUD element placement.

--- source/HUD.hx
+++ source/HUD.hx
@@ -9,6 +9,8 @@ import flixel.util.FlxColor;
 class HUD extends FlxTypedGroup<FlxSprite> {
 	static inline var HUD_HEIGHT:Int = 25;
 
+	var background:FlxSprite;
+	var tanksDestroyedIcon:FlxSprite;
 	var tanksDestroyedCounter:FlxText;
 	var levelNumber:FlxText;
 	var levelName:FlxText;
@@ -18,17 +20,30 @@ class HUD extends FlxTypedGroup<FlxSprite> {
 	public function new() {
 		super();
 
-		var background = new FlxSprite(0,
-			FlxG.height - HUD_HEIGHT).makeGraphic(FlxG.width, HUD_HEIGHT, FlxColor.GRAY);
-		add(background);
+		setupBackground();
+		setupTanksDestroyed();
 
-		var tanksDestroyedIcon = new FlxSprite(8, (background.y + (background.height / 2) - 8));
-		add(tanksDestroyedIcon);
+		/* var background = new FlxSprite(0,
+				FlxG.height - HUD_HEIGHT).makeGraphic(FlxG.width, HUD_HEIGHT, FlxColor.GRAY);
+			add(background);
 
-		tanksDestroyedCounter = new FlxText((tanksDestroyedIcon.x + 16), tanksDestroyedIcon.y, 0,
-			"BOOM", 16);
-		trace(tanksDestroyedCounter.size, tanksDestroyedCounter.height);
-		tanksDestroyedCounter.color = FlxColor.RED;
-		add(tanksDestroyedCounter);
+			var tanksDestroyedIcon = new FlxSprite(8, (background.y + (background.height / 2) - 8));
+			add(tanksDestroyedIcon);
+
+			tanksDestroyedCounter = new FlxText((tanksDestroyedIcon.x + 16), tanksDestroyedIcon.y, 0,
+				"BOOM", 16);
+			trace(tanksDestroyedCounter.size, tanksDestroyedCounter.height);
+			tanksDestroyedCounter.color = FlxColor.RED;
+			add(tanksDestroyedCounter); */
+
+		function setBackground() {
+			background = new FlxSprite(0,
+				FlxG.height - HUD_HEIGHT).makeGraphic(FlxG.width, HUD_HEIGHT, FlxColor.GRAY);
+		}
+
+		function setupTanksDestroyed() {
+			tanksDestroyedIcon = new FlxSprite();
+			/**Need to figure out midpoint.  I'll need to combine it with some manual mathing to get things to align properly.**/
+		}
 	}
 }
