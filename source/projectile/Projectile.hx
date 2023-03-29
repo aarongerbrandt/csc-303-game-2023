f2b195f71ec6d8f14d4a80ed4fe4f0033a05ece8
Sawyer <greekgeek6467@gmail.com>
2023-03-28 20:06:58 -0500

    Added projectile class and basic logic

---
+++ source/projectile/Projectile.hx
@@ -0,0 +1,31 @@
+package projectile;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+
+class Projectile extends FlxSprite {
+	private static var SPEED:Float = 250;
+
+	public function new() {
+		super(FlxG.width, FlxG.height);
+		makeGraphic(10, 10, FlxColor.BLACK);
+		kill();
+	}
+
+	override public function update(elapsed:Float) {
+		super.update(elapsed);
+	}
+
+	public function fire(startPoint:FlxPoint, aimDegrees:Float) {
+		reset(startPoint.x, startPoint.y);
+		var result = FlxPoint.weak(1, 0);
+		velocity = result.rotateByDegrees(aimDegrees).scale(SPEED);
+		angle = aimDegrees;
+	}
+
+	private function impact(surface:String) {
+		kill();
+	}
+}
