75ae96179ac306e09dcac000a8fe4eb2a6ff066a
Richard Peterson <peterson4487@live.missouristate.edu>
2023-03-30 00:15:42 -0500

    Placed HUD elements and added TODO list.

--- source/HUD.hx
+++ source/HUD.hx
@@ -12,16 +12,14 @@ class HUD extends FlxTypedGroup<FlxSprite> {
 	var background:FlxSprite;
 	var tanksDestroyedIcon:FlxSprite;
 	var tanksDestroyedCounter:FlxText;
-	var levelNumber:FlxText;
-	var levelName:FlxText;
-	var levelTanksIcon:FlxSprite;
-	var levelTanksCounter:FlxText;
 
 	public function new() {
 		super();
 
 		setupBackground();
 		setupTanksDestroyed();
+		setupLevelInformation();
+		setupLevelTanksInformation();
 	}
 
 	function setupBackground() {
@@ -32,29 +30,63 @@ class HUD extends FlxTypedGroup<FlxSprite> {
 
 	function setupTanksDestroyed() {
 		tanksDestroyedIcon = new FlxSprite(0, 0, AssetPaths.place__png);
-		tanksDestroyedIcon.x = tanksDestroyedIcon.width / 2;
+		tanksDestroyedIcon.x = 8;
 		tanksDestroyedIcon.y = background.y
 			+ (background.height / 2)
 			- (tanksDestroyedIcon.height / 2);
 
 		tanksDestroyedCounter = new FlxText();
-		tanksDestroyedCounter.x = tanksDestroyedIcon.x + 16;
-		// Still need to change this one.  Probably make it match with the x value spacing.
-
+		tanksDestroyedCounter.text = "0";
+		tanksDestroyedCounter.size = 16;
+		tanksDestroyedCounter.x = tanksDestroyedIcon.x + tanksDestroyedIcon.width + 8;
 		tanksDestroyedCounter.y = tanksDestroyedIcon.getGraphicMidpoint().y
-			- ((tanksDestroyedIcon.height / 4) + (tanksDestroyedCounter.height / 2));
-		tanksDestroyedCounter.text = "BOOM!";
-		tanksDestroyedCounter.size = 16; // Make this one a static once everything works.
-
+			- (tanksDestroyedCounter.height / 2);
 		tanksDestroyedCounter.color = FlxColor.RED;
 
 		add(tanksDestroyedIcon);
 		add(tanksDestroyedCounter);
+	}
 
-		/**
-			Still working on figuring out how to align. I'm closer than I was.
-			When background height changes, it stays aligned, but when the font size is changed, it
-			goes all out of whack.
-		**/
+	function setupLevelInformation() {
+		var levelNumber:Int = 0;
+		var levelName:String = "Insert Witty Level Name Here!";
+		var levelInformation = new FlxText();
+
+		levelInformation.text = "Level " + levelNumber + ": " + levelName;
+		levelInformation.size = 16;
+		levelInformation.x = background.getGraphicMidpoint().x - (levelInformation.width / 2);
+		levelInformation.y = tanksDestroyedCounter.y;
+		levelInformation.color = FlxColor.RED;
+
+		add(levelInformation);
+	}
+
+	function setupLevelTanksInformation() {
+		var levelTanksStart:Int = 0;
+		var levelTanksRemain:Int = 0;
+		var levelTanksIcon = new FlxSprite(0, 0, AssetPaths.place__png);
+		var levelTanksInformation = new FlxText();
+
+		levelTanksInformation.text = levelTanksRemain + " / " + levelTanksStart;
+		levelTanksInformation.size = 16;
+		levelTanksInformation.x = FlxG.width - levelTanksInformation.width - 8;
+		levelTanksInformation.y = tanksDestroyedCounter.y;
+		levelTanksInformation.color = FlxColor.RED;
+
+		levelTanksIcon.x = levelTanksInformation.x - levelTanksIcon.width - 8;
+		levelTanksIcon.y = tanksDestroyedIcon.y;
+
+		add(levelTanksIcon);
+		add(levelTanksInformation);
 	}
 }
+/**
+	TODO: Move icon and font sizes to static.
+	TODO: Tie HUD_HEIGHT to block size, when the HUD is called for.
+	TODO: Move magic 8s to either static or dynamic.
+	TODO: Replace placeholder images. Not sure on artistic abilities.
+	TODO: Create update Tanks Destroyed function.
+	TODO: Create update Level Number function.
+	TODO: Create Random Level Name Generator function.
+	TODO: Create update Level Enemy Start and Remaining function(s).
+**/
